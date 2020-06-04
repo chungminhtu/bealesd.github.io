512a410febc0fb3ec040645e51fd51f4cbcba5e9
David Beales <David.Beales@ukho.gov.uk>
2020-06-04 08:22:44 +0100

    Revert changes of last 2 commits.

--- main.js
+++ main.js
@@ -544,8 +544,7 @@ function swapJsonKeyValues(json) {
 }
 
 function getUrlSuffix() {
-	// const url = window.location.href;
-	const url = window.redirectUrl === undefined ? "" : window.redirectUrl;
+	const url = window.location.href;
 	const urlSuffixRegex = url.match(/[A-Za-z]+\.html/);
 	return urlSuffixRegex !== null ? urlSuffixRegex[0].slice(0, -5) : "";
 }
